0d67bc189a808ce46eb7cb923df0661bfb4b21b5
Marian Ban <marian.ban@gmail.com>
2019-11-27 20:43:48 +0100

    custom cypress github action

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ const homeDirectory = path.join(
   core.getInput('package-directory')
 )
 
+console.log('home directory', homeDirectory)
+
 const useYarn = fs.existsSync('yarn.lock')
 const lockFilename = useYarn
   ? 'yarn.lock'
