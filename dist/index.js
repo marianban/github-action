304ae149d4459d82fb0f4895ae92e9071662593a
Marian Ban <marian.ban@gmail.com>
2019-12-02 20:26:08 +0100

    new release

--- dist/index.js
+++ dist/index.js
@@ -2436,7 +2436,7 @@ const homeDirectory = path.join(
   core.getInput('package-directory')
 )
 
-console.log('home directory', homeDirectory)
+console.log('home directory ffs', homeDirectory)
 
 const useYarn = fs.existsSync('yarn.lock')
 const lockFilename = useYarn
